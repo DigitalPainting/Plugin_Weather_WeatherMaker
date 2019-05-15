84fcdf415d48ae1058deb2a5e3cff23c22b4f35b
Ross Gardler <rgardler@apache.org>
2019-05-15 15:59:42 -0700

    refactor the day night plugin and restructure the entire tree

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ To include this plugin in your Digital Painting use the following:
 cd Assets
 git submodule add git@github.com:DigitalPainting/WeatherMakerPlugin.git
 ```
+
+## Use
+
+  1. Create a configuration file using `Assets -> Create -> Wizards Code/PLUGIN TYPE ... ` There will be entries in this folder for each of the plugins you have installed
+  2. Drag the generated configuration file into the `Configuration` field of the `PLUGIN Manager` component attached to the `DigitalPaintingManager` game object
+  3. Double click the configuration file and configure as appropriate
+
+## Available Plugins
+
+Below you will find a list of available plugins along with an indication of any dependency they have. Note that some plugins fit into multiple categories and are thus listed multiple times.
+
+### Day Night Cycle
+
+These plugins provide a day night cycle that changes the lighting according to the time of day in the scene. The Digital Painting comes with a very basic Day/Night Cycle, the plugins here will provide a much better lucking experience.
+
+#### Digital Ruby's Weather Maker
+
+[Digital Ruby's Weather Maker](https://assetstore.unity.com/packages/tools/particles-effects/weather-maker-sky-weather-water-volumetric-light-60955) includes an advanced Day Night cycle. Please purchase and install this asset first.
+
+### Weather 
+
+The following plugins add weather systems to The Digital Painting.
+
+#### Digital Ruby's Rain Maker (Free)
+
---
+++ Weather_WeatherMaker/Scripts/WeatherMakerWeatherSystem.cs
@@ -0,0 +1,155 @@
+﻿using DigitalRuby.WeatherMaker;
+using UnityEngine;
+
+namespace wizardscode.environment.weather
+{
+    [CreateAssetMenu(fileName = "WeatherMakerWeatherSystem", menuName = "Wizards Code/Weather/Weather Maker")]
+    public class WeatherMakerWeatherSystem : AbstractWeatherSystem
+    {
+        [Header("Base Profiles for Weather Maker")]
+        [Tooltip("Automated weather profile. If this is null then either manual or manager controlled weather is used. If this has a profile then it will override all other settings.")]
+        public WeatherMakerProfileGroupScript automatedGroupProfile;
+        [Tooltip("Profile for a clear weather.")]
+        public WeatherMakerProfileScript clearProfile;
+        [Tooltip("Profile for rainy weather.")]
+        public WeatherMakerProfileScript rainProfile;
+        [Tooltip("Profile for snowy weather")]
+        public WeatherMakerProfileScript snowProfile;
+        [Tooltip("Profile for sleet.")]
+        public WeatherMakerProfileScript sleetProfile;
+        [Tooltip("Profile for hail.")]
+        public WeatherMakerProfileScript hailProfile;
+
+        private WeatherManager manager;
+        private GameObject weatherMaker;
+        private WeatherMakerWeatherZoneScript zone;
+
+        private GameObject weather;
+        private float timeToNextUpdate;
+        private DayNightCycleManager dayNightManager;
+
+        public WeatherMakerProfileScript CurrentWeatherMakerProfile
+        {
+            get; set;
+        }
+
+        internal override void Initialize()
+        {
+            RenderSettings.fog = false;
+            RenderSettings.skybox = skyboxMaterial;
+
+
+        }
+
+        internal override void Start()
+        {
+            manager = FindObjectOfType<WeatherManager>();
+            if (manager == null)
+            {
+                Debug.LogError("Cannot find Weather Manager.");
+            }
+
+            zone = FindObjectOfType<WeatherMakerWeatherZoneScript>();
+            if (zone == null)
+            {
+                Debug.LogError("Unable to fine a WeatherMakerWeatherZoneScript");
+            }
+
+
+            WeatherMakerScript component = GameObject.FindObjectOfType<WeatherMakerScript>();
+            if (component == null)
+            {
+                Debug.LogError("You don't have a WeatherMakerScript in your scene. Please see the Weather Maker Weather System plugin README for instructions.");
+                return;
+            }
+            else
+            {
+                weatherMaker = component.gameObject;
+            }
+
+            if (weatherMaker == null)
+            {
+                Debug.LogError("Unable to find or instantiate an object with the WeatherMakerScript attached");
+            }
+
+            DontDestroyOnLoad(weatherMaker);
+
+            if (automatedGroupProfile != null)
+            {
+                zone.ProfileGroup = automatedGroupProfile;
+                zone.SingleProfile = null;
+                manager.isAuto = false;
+
+                float transisitonDuration = 15f;
+                float holdDuration = 20f;
+
+                dayNightManager = FindObjectOfType<DayNightCycleManager>();
+                if (dayNightManager != null)
+                {
+                    transisitonDuration = dayNightManager.GameSecondsToRealSeconds(120 * 60); // 2 hour game time
+                    holdDuration = dayNightManager.GameSecondsToRealSeconds(15 * 60); // 1/4 hour game time
+                }
+
+                automatedGroupProfile.TransitionDuration.Minimum = transisitonDuration * 0.75f;
+                automatedGroupProfile.TransitionDuration.Maximum = transisitonDuration * 1.25f;
+                automatedGroupProfile.HoldDuration.Minimum = holdDuration * 0.75f;
+                automatedGroupProfile.HoldDuration.Maximum = holdDuration * 1.25f;
+            }
+
+            // Since we've changed the config of the weather maker manager we need to trigger the OnEnable method so that it re-initializes
+            weatherMaker.SetActive(false);
+            weatherMaker.SetActive(true);
+
+            WeatherMakerScript.Instance.RaiseWeatherProfileChanged(null, clearProfile, 1, 20, true, null);
+        }
+
+        internal override void Update()
+        {
+            if (CurrentProfile.isDirty)
+            {
+                switch (CurrentProfile.PrecipitationType)
+                {
+                    case WeatherProfile.PrecipitationTypeEnum.Clear:
+                        ChangeWeather(clearProfile);
+                        break;
+                    case WeatherProfile.PrecipitationTypeEnum.Rain:
+                        rainProfile.PrecipitationProfile.IntensityRange = new RangeOfFloats(CurrentProfile.PrecipitationIntensity * 0.8f, CurrentProfile.PrecipitationIntensity);
+                        ChangeWeather(rainProfile);
+                        break;
+                    case WeatherProfile.PrecipitationTypeEnum.Hail:
+                        rainProfile.PrecipitationProfile.IntensityRange = new RangeOfFloats(CurrentProfile.PrecipitationIntensity * 0.8f, CurrentProfile.PrecipitationIntensity);
+                        ChangeWeather(hailProfile);
+                        break;
+                    case WeatherProfile.PrecipitationTypeEnum.Sleet:
+                        rainProfile.PrecipitationProfile.IntensityRange = new RangeOfFloats(CurrentProfile.PrecipitationIntensity * 0.8f, CurrentProfile.PrecipitationIntensity);
+                        ChangeWeather(sleetProfile);
+                        break;
+                    case WeatherProfile.PrecipitationTypeEnum.Snow:
+                        rainProfile.PrecipitationProfile.IntensityRange = new RangeOfFloats(CurrentProfile.PrecipitationIntensity * 0.8f, CurrentProfile.PrecipitationIntensity);
+                        ChangeWeather(snowProfile);
+                        break;
+                    default:
+                        Debug.LogError("WeatherMakerWeatherSystem does not know how to handle precipitation type '" + CurrentProfile.PrecipitationType + "' using clear profile");
+                        ChangeWeather(clearProfile);
+                        break;
+                }
+
+                CurrentProfile.isDirty = false;
+            }
+        }
+
+        private void ChangeWeather(WeatherMakerProfileScript newProfile)
+        {
+            if (WeatherMakerScript.Instance == null)
+            {
+                Debug.LogWarning("WeatherMakerScript instance not initialized correctly");
+            }
+            else
+            {
+                WeatherMakerProfileScript lastProfile = CurrentWeatherMakerProfile;
+                CurrentWeatherMakerProfile = newProfile;
+                WeatherMakerScript.Instance.RaiseWeatherProfileChanged(lastProfile, newProfile, 20, UnityEngine.Random.value * 20 + 10, true, null);
+            }
+        }
+    }
+}
